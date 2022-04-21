caaa889a4144d69527e652b934e6a405616750d6
Melvyn Linke <melvyn.linke@haw-hamburg.de>
2022-04-21 17:54:25 +0200

    Test fix subfolder add

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='AgentRoutingSimulator',
-    packages=find_packages(),
+    packages=find_packages("AgentRoutingSimulator"),
     version='0.1',
     license='GLP3',
     description='TYPE YOUR DESCRIPTION HERE',
